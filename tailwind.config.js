0094f779b1db7816b1453f4875c849dbdd1022b6
Oussama Souayri <souayri.oussama@gmail.com>
2022-09-14 19:44:52 +0100

    tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,10 @@
-// /** @type {import('tailwindcss').Config} */
-// module.exports = {
-//   content: [
-//     "./src/**/*.{html,ts}",
-//   ],
-//   theme: {
-//     extend: {},
-//   },
-//   plugins: [],
-// }
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,ts}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
