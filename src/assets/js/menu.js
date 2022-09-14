03c07d2628d7b75dbe1d33514658a277492adc51
Oussama Souayri <souayri.oussama@gmail.com>
2022-09-14 19:35:20 +0100

    create portfolio project angular

---
+++ src/assets/js/menu.js
@@ -0,0 +1,21 @@
+// setTimeout(() => {
+//     (function(){"use strict";
+
+// const menuToggler=document.querySelector(".menu-toggler");
+// const menuWrapper=document.querySelector(".menu-wrapper");
+// menuToggler.addEventListener("click",()=>{
+//     menuWrapper.classList.toggle("hidden");
+//     document.body.classList.toggle("overflow-y-hidden");
+//     menuToggler.querySelector(".cross").classList.toggle("hidden");
+//     menuToggler.querySelector(".menu").classList.toggle("hidden");
+// });
+// document.querySelectorAll(".navbar li:not(.submenu-item) a").forEach((e)=>e.addEventListener("click",()=>{menuWrapper.classList.toggle("flex");
+// document.body.classList.toggle("overflow-y-hidden");
+// menuToggler.querySelector(".cross").classList.toggle("hidden");
+// menuToggler.querySelector(".menu").classList.toggle("hidden");
+// }));
+// })();
+// }, 1000);
+
+    
+    
