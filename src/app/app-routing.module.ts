4bf12438f00c5d8cbc14c6124dae6eb5bf1190d2
Oussama Souayri <souayri.oussama@gmail.com>
2022-09-15 18:59:48 +0100

    some update in ts menu toggler

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,12 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
-  // {path:'', redirectTo:'home', pathMatch:'full' },
-  // {
-  //   path: 'home',
-  //   component: HomeComponent
-  // },
-  // { path: '**', redirectTo: 'home'},
+  {path:'', redirectTo:'home', pathMatch:'full' },
+  {
+    path: 'home',
+    component: HomeComponent
+  },
+  { path: '**', redirectTo: 'home'},
 ];
 
 @NgModule({
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,3 +8,16 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'portfolio';
 }
+setTimeout(() => {
+const menuToggler=document.querySelector(".menu-toggler") as HTMLInputElement;
+const menuWrapper=document.querySelector(".menu-wrapper") as HTMLInputElement;
+menuToggler.addEventListener('click', () => menu());
+const closeicon = menuToggler.querySelector(".cross") as HTMLInputElement;
+const menuicon = menuToggler.querySelector(".menu") as HTMLInputElement;
+function menu(){
+    menuWrapper.classList.toggle("hidden");
+    document.body.classList.toggle("overflow-y-hidden");
+    closeicon.classList.toggle("hidden");
+    menuicon.classList.toggle("hidden");
+}
+}, 1);
